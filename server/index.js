d06504b947e733071255b6f3dedef495447f8fc7
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2025-10-08 13:04:50 +0530

    added methods to cors

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ const app=express();
 app.use(
   cors({
     origin: "http://localhost:5173", 
-    credentials: true,               
+     methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
   })
 );
 app.use(express.json())
