ee05c417fec2b4acd0f8623408f6ed20c5e39806
Marmkumar Akhiyaniya <marm.soni2710@gmail.com>
2025-10-08 13:04:20 +0530

    added backend online api for all

--- client/api/api.js
+++ client/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-assistant-online-marmkumars-projects.vercel.app/api",
   withCredentials: true, 
 });
 
